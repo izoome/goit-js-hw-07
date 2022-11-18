f05e44514287288eb7162cf7845e12e800e92b52
izoome <serhii.mihalich@gmail.com>
2022-11-18 15:41:07 +0200

    update

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from "./gallery-items.js";
+
+const galleryListItem = document.querySelector(".gallery");
+// const galleryListItem = document.querySelectorAll('.gallery');
+
+const createListItem = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+// galleryListItem.addEventListener("click", () => onImageClick);
+
+galleryListItem.addEventListener("click", onImageClick);
+
+galleryListItem.insertAdjacentHTML("afterbegin", `${createListItem}`);
+
+function onImageClick(image) {
+  image.preventDefault();
+  if (image.target === image.currentTarget) return;
+
+  const html = `<img src="${image.target.dataset.source}">`;
+
+  const modalLightbox = basicLightbox.create(html, {
+    onShow: () => {
+      document.addEventListener("keydown", onModalLightboxClose);
+      console.log(`addEventListener`);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", onModalLightboxClose);
+      console.log(`removeEventListener`);
+    },
+  });
+
+  modalLightbox.show();
+
+  function onModalLightboxClose(event) {
+    if (event.code === "Escape") {
+      modalLightbox.close();
+      console.log(`testing a function`);
+    }
+  }
+}
