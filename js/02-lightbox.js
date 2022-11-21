7f75bcdad38b35c1f21720008866597b676c9be6
izoome <serhii.mihalich@gmail.com>
2022-11-21 10:32:42 +0200

    update-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,5 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+
+const galleryListItem = document.querySelector(".gallery");
+
+const createGalleryListItem = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("");
+
+galleryListItem.insertAdjacentHTML("afterbegin", `${createGalleryListItem}`);
+
+const gallerySimpleLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
-
